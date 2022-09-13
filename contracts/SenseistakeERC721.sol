8a8e98260c3e6a5054cada51b3a395cb533584a5
carloscip <cip@MacBook-Pro-de-CIP.local>
2022-09-13 16:59:46 -0300

    docs ready for erc721 in natspec format

--- contracts/SenseistakeERC721.sol
+++ contracts/SenseistakeERC721.sol
@@ -19,17 +19,28 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
     using Address for address;
     using Address for address payable;
 
+    /// @notice IPFS base uri
     string private _baseUri =
         "ipfs://QmWMi519m7BEEdNyxsmadLC214QzgXRemp3wa2pzw95Gm4/";
 
+    /// @notice Deposit amount required for being a client
     uint256 private constant FULL_DEPOSIT_SIZE = 32 ether;
+
+    /// @notice Scale for getting the commission rate (service fee)
     uint32 private constant COMMISSION_RATE_SCALE = 1_000_000;
 
+    /// @notice Template service contract implementation address
+    /// @dev It is used for generating clones, using hardhats proxy clone
+    /// @return servicesContractImpl where the service contract template is implemented
     address payable public servicesContractImpl;
 
+    /// @notice Used in conjuction with `COMMISSION_RATE_SCALE` for determining service fees
+    /// @dev Is set up on the constructor and can be modified with provided setter aswell
+    /// @return commissionRate the commission rate
     uint32 public commissionRate;
 
-    // for only allowing mint once per salt
+    /// @notice Determines if a certain tokenId was minted
+    /// @dev For allowing only a single mint per service contract
     mapping(bytes32 => bool) internal minted;
 
     event ServiceImplementationChanged(
@@ -58,9 +69,6 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         string memory symbol_,
         uint32 commissionRate_
     ) ERC721(name_, symbol_) {
-        if (commissionRate_ > COMMISSION_RATE_SCALE) {
-            revert CommissionRateScaleExceeded(commissionRate_);
-        }
         if (commissionRate_ > (COMMISSION_RATE_SCALE / 2)) {
             revert CommisionRateTooHigh(commissionRate_);
         }
@@ -119,14 +127,13 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         ) {
             revert BurnInvalid();
         }
-        // burns token of salt
         _burn(uint256(salt_));
     }
 
+    /// @notice Changes commission rate (senseistake service fees)
+    /// @dev Cannot be more than 50% commission
+    /// @param commissionRate_ New commission rate
     function changeCommissionRate(uint32 commissionRate_) external onlyOwner {
-        if (commissionRate_ > COMMISSION_RATE_SCALE) {
-            revert CommissionRateScaleExceeded(commissionRate_);
-        }
         if (commissionRate_ > (COMMISSION_RATE_SCALE / 2)) {
             revert CommisionRateTooHigh(commissionRate_);
         }
@@ -134,6 +141,10 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         emit CommissionRateChanged(commissionRate_);
     }
 
+    /// @notice Creates service contract based on implementation
+    /// @dev Performs a clone of the implementation contract, a service contract handles logic for managing user deposit, withdraw and validator
+    /// @param salt_ Service contract salt
+    /// @param operatorDataCommitment_ Operator data commitment
     function createContract(bytes32 salt_, bytes32 operatorDataCommitment_)
         external
         payable
@@ -167,6 +178,9 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         }
     }
 
+    /// @notice Allows deposits into different service contracts
+    /// @dev According to salts and `msg.value` provided it will be performing deposits up untill no more ether left (or salts)
+    /// @param salts_ Salts that derive in service contracts addresses
     function fundMultipleContracts(bytes32[] calldata salts_) external payable {
         if (msg.value < FULL_DEPOSIT_SIZE) {
             revert ValueSentLowerThanMinimumDeposit();
@@ -175,6 +189,7 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         uint256 remaining = msg.value;
         address depositor = msg.sender;
         uint8 saltsLen = uint8(salts_.length);
+
         for (uint8 i; i < saltsLen; ) {
             if (remaining == 0) break;
             address proxy = Clones.predictDeterministicAddress(
@@ -212,6 +227,9 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         }
     }
 
+    /// @notice Performs withdraw of balance in service contract
+    /// @dev The `tokenId_` is a cast of the salt, and thus we can access the service contract from which the owner can perform a withdraw (if possible)
+    /// @param tokenId_ Is the salt casted to uint256
     function withdraw(uint256 tokenId_) external {
         address proxy = Clones.predictDeterministicAddress(
             servicesContractImpl,
@@ -226,6 +244,10 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         serviceContract.withdrawTo(payable(msg.sender));
     }
 
+    /// @notice Gets service contract address
+    /// @dev For getting the service contract address of a given salt
+    /// @param salt_ Is the service contract salt
+    /// @return Address of a service contract
     function getServiceContractAddress(bytes32 salt_)
         external
         view
@@ -234,18 +256,27 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         return Clones.predictDeterministicAddress(servicesContractImpl, salt_);
     }
 
-    function saltToTokenId(bytes32 salt_) external pure returns (uint256 salt) {
-        salt = uint256(salt_);
+    /// @notice Casts bytes32 salt to uint256 tokenId
+    /// @param salt_ Is the service contract salt
+    /// @return tokenId Is the salt casted to uint256
+    function saltToTokenId(bytes32 salt_) external pure returns (uint256 tokenId) {
+        tokenId = uint256(salt_);
     }
 
+    /// @notice Casts uint256 tokenId to bytes32 salt
+    /// @param tokenId_ Is the uint256 casted salt
+    /// @return salt Is the tokenId casted to bytes32
     function tokenIdToSalt(uint256 tokenId_)
         external
         pure
-        returns (bytes32 tokenId)
+        returns (bytes32 salt)
     {
-        tokenId = bytes32(tokenId_);
+        salt = bytes32(tokenId_);
     }
 
+    /// @notice Gets token uri where the metadata of NFT is stored
+    /// @param tokenId_ Is the uint256 casted salt
+    /// @return Token uri of the tokenId provided
     function tokenURI(uint256 tokenId_)
         public
         view
@@ -255,17 +286,18 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId_);
     }
 
-    // function _baseURI() internal view virtual override returns (string memory) {
-    //     return "https://ipfs.io/ipfs/QmSiQuffUmDf3TsNRGApBiMkgTc8cLipAVcWM4V7kdTyBo?filename=";
-    // }
-
+    /// @notice Lifecycle hook for every transfer, mint or burn done of the erc721 token
+    /// @dev Only needed to change the depositor address on transfers, for mint/burn this is handled in the service contract
+    /// @param from_ Address of the owner
+    /// @param to_ Address of the receiver
+    /// @param tokenId_ Is the uint256 casted salt
     function _afterTokenTransfer(
         address from_,
         address to_,
         uint256 tokenId_
     ) internal virtual override(ERC721) {
         super._afterTokenTransfer(from_, to_, tokenId_);
-        // transfer
+        // transfer operation
         if (to_ != address(0) && from_ != address(0)) {
             address proxy = Clones.predictDeterministicAddress(
                 servicesContractImpl,
@@ -278,6 +310,8 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         }
     }
 
+    /// @notice For removing ownership of an NFT from a wallet address
+    /// @param tokenId_ Is the uint256 casted salt
     function _burn(uint256 tokenId_)
         internal
         override(ERC721, ERC721URIStorage)
@@ -285,6 +319,9 @@ contract SenseistakeERC721 is ERC721, ERC721URIStorage, Ownable {
         super._burn(tokenId_);
     }
 
+    /// @notice Returns min value of two provided (if equality returns first)
+    /// @param a_ The first value
+    /// @param b_ The second value
     function _min(uint256 a_, uint256 b_) internal pure returns (uint256) {
         return a_ <= b_ ? a_ : b_;
     }
