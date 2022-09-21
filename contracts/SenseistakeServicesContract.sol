e0fc0b38b50478377a8fa9f4088fb8bc971913c0
carloscip <cip@MacBook-Pro-de-CIP.local>
2022-09-21 11:05:00 -0300

    fee optimization and removed unused part of if endOperatorServices

--- contracts/SenseistakeServicesContract.sol
+++ contracts/SenseistakeServicesContract.sol
@@ -151,11 +151,10 @@ contract SenseistakeServicesContract is Initializable {
             revert NotAllowedInCurrentState();
         }
         if (
-            (msg.sender == Ownable(tokenContractAddress).owner() &&
-                block.timestamp < exitDate) ||
             (msg.sender == SenseiStake(tokenContractAddress).ownerOf(tokenId) &&
                 block.timestamp < exitDate) ||
-            (msg.sender == tokenContractAddress && block.timestamp < exitDate)
+            (msg.sender == Ownable(tokenContractAddress).owner() &&
+                block.timestamp < exitDate)
         ) {
             revert NotAllowedAtCurrentTime();
         }
